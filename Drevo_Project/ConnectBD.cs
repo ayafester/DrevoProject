31d07cb4fce6235c327f0baec01551ea0458cd54
Ayaofthshire <ayanka_m@mail.ru>
2020-11-12 14:45:38 +0300

    добавила поколение в бд

--- Drevo_Project/ConnectBD.cs
+++ Drevo_Project/ConnectBD.cs
@@ -43,7 +43,7 @@ namespace Drevo_Project
 
                 command.CommandText = "CREATE TABLE IF NOT EXISTS Card (id INTEGER PRIMARY KEY AUTOINCREMENT, surname TEXT, name TEXT, middlename TEXT, gender INTEGER, " +
                     "bio TEXT, birthday TEXT, deathday TEXT, number TEXT, idCreator INTEGER REFERENCES User(id), idMom INTEGER REFERENCES Card(id) DEFAULT 0, " +
-                    "idDad INTEGER REFERENCES Card(id) DEFAULT 0, idPartner INTEGER REFERENCES Card(id) DEFAULT 0, PhotoOnAva TEXT)";
+                    "idDad INTEGER REFERENCES Card(id) DEFAULT 0, idPartner INTEGER REFERENCES Card(id) DEFAULT 0, PhotoOnAva TEXT, Generation INTEGER)";
                 command.ExecuteNonQuery();
 
                 command.CommandText = "CREATE TABLE IF NOT EXISTS Photos (id INTEGER PRIMARY KEY AUTOINCREMENT, link TEXT, idCard INTEGER REFERENCES Card(id) DEFAULT 0)"; //Создали таблицу Фото ... если много фото?
